f467069af0988c45bf79a96fefe408e1f8dc2513
Murtaza Abbas <132145976+Murtaza7863@users.noreply.github.com>
2026-05-24 10:26:24 +0800

    Bulk sync: 18 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        table = {}
+        for c in s:
+            table[c] = table.get(c, 0) + 1
+        
+        for c in t:
+            if c not in table or table[c] == 0:
+                return False
+            table[c] -= 1
+        
+        return True
